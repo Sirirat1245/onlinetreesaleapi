318bb2c198fc33454a6b3c0ab6bc135f0ff83a7d
pornnapha1998 <pornnapha.noiin@gmail.com>
2021-08-01 21:33:01 +0700

    first push api

---
+++ src/main/java/onlinesale/onlinetree/OnlinetreeApplication.java
@@ -0,0 +1,13 @@
+package onlinesale.onlinetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinetreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinetreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/onlinesale/onlinetree/OnlinetreeApplicationTests.java
@@ -0,0 +1,13 @@
+package onlinesale.onlinetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinetreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
